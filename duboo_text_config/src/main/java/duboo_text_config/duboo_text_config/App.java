e50694157755cec66ea301f9228b5e724b3cacf0
niuchen <41614969@qq.com>
2018-03-08 10:03:10 +0800

    两个duboo例子. 一个是自己写的 一个是别人的不完整代码duboodome  使用了spring  mvc 既是服务提供者又是消费者  zookeeper注册中心  里面用的了参数验证的例子.  duboo注册中心 的配置有个config工程 专门配置

---
+++ duboo_text_config/src/main/java/duboo_text_config/duboo_text_config/App.java
@@ -0,0 +1,13 @@
+package duboo_text_config.duboo_text_config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
